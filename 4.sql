e61891d85cb2d324433e2b4520fb1d216e58cb61
HalyshAnton <kyhar007@gmail.com>
2026-06-02 20:30:17 +0300

    add lesson4

--- 4.sql
+++ 4.sql
@@ -28,26 +28,38 @@
 -- )
 
 
-INSERT INTO STUDENTS (NAME, AGE, GROUP_ID) VALUES
-('Ivan Petrenko', 18, 1),
-('Oleh Shevchenko', 19, 1),
-('Maria Kovalenko', 18, 2),
-('Anna Tkachenko', 20, 2),
-('Dmytro Bondar', 21, 3),
-('Sofia Melnyk', 19, 3),
-('Andrii Koval', 22, 4),
-('Iryna Lysenko', 18, 4),
-('Viktor Marchenko', 20, 5),
-('Olena Sydorenko', 19, 5),
+-- INSERT INTO STUDENTS (NAME, AGE, GROUP_ID) VALUES
+-- ('Ivan Petrenko', 18, 1),
+-- ('Oleh Shevchenko', 19, 1),
+-- ('Maria Kovalenko', 18, 2),
+-- ('Anna Tkachenko', 20, 2),
+-- ('Dmytro Bondar', 21, 3),
+-- ('Sofia Melnyk', 19, 3),
+-- ('Andrii Koval', 22, 4),
+-- ('Iryna Lysenko', 18, 4),
+-- ('Viktor Marchenko', 20, 5),
+-- ('Olena Sydorenko', 19, 5),
 
-('Taras Shevchuk', 18, 1),
-('Kateryna Hrytsenko', 21, 2),
-('Yaroslav Kuts', 22, 3),
-('Natalia Romanenko', 19, 4),
-('Bohdan Filipov', 20, 5),
+-- ('Taras Shevchuk', 18, 1),
+-- ('Kateryna Hrytsenko', 21, 2),
+-- ('Yaroslav Kuts', 22, 3),
+-- ('Natalia Romanenko', 19, 4),
+-- ('Bohdan Filipov', 20, 5),
 
-('Alina Zaitseva', 18, 1),
-('Maksym Danyliuk', 21, 2),
-('Veronika Ostapchuk', 20, 3),
-('Pavlo Savchuk', 19, 4),
-('Diana Moroz', 22, 5);
+-- ('Alina Zaitseva', 18, 1),
+-- ('Maksym Danyliuk', 21, 2),
+-- ('Veronika Ostapchuk', 20, 3),
+-- ('Pavlo Savchuk', 19, 4),
+-- ('Diana Moroz', 22, 5);
+
+-- -- УТОЧНЕННЯ З ЯКОЇ ТАБЛИЦІ СТОВПЧИК
+-- -- [ТАБЛИЦЯ].[СТОВПЧИК]
+-- SELECT G.NAME
+-- FROM GROUPS G -- СКОРОЧЕННЯ ДЛЯ НАЗВИ ТАБЛИЦІ
+
+
+-- ОБ'ЄДНАННЯ ТАБЛИЦЬ
+-- ВИВЕСТИ СТУДЕНТІВ ТА ЇХНІ ГРУПИ
+-- [ТАБЛИЦЯ 1] JOIN [ТАБЛИЦЯ 2] ON [УМОВА] JOIN [ТАБЛИЦЯ 3] ON [УМОВА] ...
+SELECT *
+FROM GROUPS G JOIN STUDENTS S ON S.GROUP_ID = G.ID
